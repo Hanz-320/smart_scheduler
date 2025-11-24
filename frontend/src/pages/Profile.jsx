a1169236413199c73b97165cd3ddb3d8d97e8b10
Hanz-320 <hanzng031@gmail.com>
2025-11-24 23:18:12 +0800

    update profile to work on website

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -5,7 +5,7 @@ import EditProfileModal from "../components/EditProfileModal";
 import Notification from "../components/Notification";
 import axios from "axios";
 
-const BACKEND_URL = "http://localhost:5000";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_API_URL || "http://localhost:5000";
 
 export default function Profile({ user }) {
   const navigate = useNavigate();
